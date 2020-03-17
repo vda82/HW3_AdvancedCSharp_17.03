bc965d509e1cf744eb6609b28737e2e7e262798d
vitaliy_dryha <Vitaliy_Dryha@epam.com>
2020-03-17 16:09:23 +0100

    Add project files.

---
+++ HW3_AdvancedCSharp_17.03/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_AdvancedCSharp_17._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> allDirsFilesList = new List<string>();
+            string path = @"C:\Users\Vitaliy_Dryha\Desktop\C# Anton";
+            FileSystemVisitor fileSystemVisitor = new FileSystemVisitor("txt");
+
+
+
+            Start start = new Start();
+            fileSystemVisitor.SearchStarted += start.OnSearchStarted;
+
+
+
+            foreach (var dirOrFile in fileSystemVisitor.GetAllDirectoriesAndFiles(path))
+            {
+                allDirsFilesList.Add(dirOrFile);
+            }
+
+            IEnumerable<string> query = from file in allDirsFilesList
+                                        where file.Contains(fileSystemVisitor.Filter)
+                                        select file;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class FileSystemVisitor
+    {
+        private string filter;
+        public string Filter { get; set; }
+        public FileSystemVisitor(string filter)
+        {
+            this.Filter = filter;
+        }
+
+        public delegate void SearchStartedEventHandler(object source, EventArgs args);
+
+        public event SearchStartedEventHandler SearchStarted;
+
+
+        public IEnumerable<string> GetAllDirectoriesAndFiles(string path)
+        {
+            OnSearchStarted();
+
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                yield return file;
+            }
+
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                yield return dir;
+            }
+            OnSearchStarted();          
+        }
+
+        protected virtual void OnSearchStarted()
+        {
+            if (SearchStarted != null)
+            {
+                SearchStarted(this, EventArgs.Empty);
+            }
+        }
+
+    }
+
+    public class Start
+    {
+        public void OnSearchStarted(object source, EventArgs args)
+        {
+            Console.WriteLine("Search Started");
+        }
+    }
+}
+
+
+
